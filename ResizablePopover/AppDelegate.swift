96e7863094d81d969a2a5807ce6bdfa1fed43c7f
will <willsuosuo@gmail.com>
2025-05-29 15:19:47 +0800

    feat(UI): add resizable popover example

--- ResizablePopover/AppDelegate.swift
+++ ResizablePopover/AppDelegate.swift
@@ -11,6 +11,26 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet var window: NSWindow!
+    
+    private lazy var contentViewController: NSViewController = {
+        let contentView = NSView()
+        let textField = NSTextField(labelWithString: "This is a resizable popover example.")
+        contentView.addSubview(textField)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            textField.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            textField.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+        let result = NSViewController()
+        result.view = contentView
+        return result
+    }()
+    
+    private lazy var popoverController: ResizablePopover = {
+        let result = ResizablePopover(minSize: NSMakeSize(300, 300), maxSize: NSMakeSize(600, 600))
+        result.contentViewController = contentViewController
+        return result
+    }()
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
@@ -26,5 +46,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
 
+    @IBAction func showPopover(_ sender: NSButton) {
+        popoverController.show(relativeTo: sender.bounds, of: sender, preferredEdge: .maxY)
+    }
 }
 
