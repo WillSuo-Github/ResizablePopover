1f61789bfd8aa4479a0691d484aed7703179e909
will <willsuosuo@gmail.com>
2025-05-29 15:15:50 +0800

    Initial Commit

---
+++ ResizablePopoverTests/ResizablePopoverTests.swift
@@ -0,0 +1,17 @@
+//
+//  ResizablePopoverTests.swift
+//  ResizablePopoverTests
+//
+//  Created by will Suo on 2025/5/29.
+//
+
+import Testing
+@testable import ResizablePopover
+
+struct ResizablePopoverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
